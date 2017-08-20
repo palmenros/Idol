203847ab2b360f02d6280f9d1a5729a588be821a
Pedro Palacios Almendros <palmenros@gmail.com>
2017-08-20 20:24:53 +0200

    Fixed PROJECT_DIR macro. Added support for linux

--- core/id/Common.h
+++ core/id/Common.h
@@ -4,4 +4,4 @@
 #define LOG(x) (std::cout << x << std::endl)
 #define ERROR(x) (std::cerr << (x) << std::endl)
 
-#define PROJECT_DIR(path) __PROJECT_DIR__ ## path
+#define PROJECT_DIR(path) (__PROJECT_DIR__ path)
